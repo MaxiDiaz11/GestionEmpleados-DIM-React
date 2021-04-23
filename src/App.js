c5dc3399d7d357733e80727e18fcf9d94f999316
Maxi Diaz <maxi.diaz66@gmail.com>
2021-04-23 10:06:38 -0300

    Se agrego la funcionalidad de imprimir varias tablas

--- src/App.js
+++ src/App.js
@@ -1,26 +1,25 @@
-import React, {useEffect, useState} from 'react';
-import InformeDiario from './components/InformeDiario';
-import axios from 'axios';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import InformeDiario from "./components/InformeDiario";
+import axios from "axios";
+import "./App.css";
 
 function App() {
-  const [informe, setInforme] = useState([]);
+  const [grupos, setGrupos] = useState([]);
 
   useEffect(() => {
     const obtenerInforme = () => {
       const consultarApi = async () => {
-        const url = 'http://127.0.0.1:3000/informeDiario/generarInforme';
+        const url = "http://127.0.0.1:3000/informeDiario/generarInforme";
         const resultado = await axios.get(url);
-        setInforme(resultado.data.empleado);
+        console.log(resultado.data.grupos.grupo);
+        setGrupos(resultado.data.grupos.grupo);
       };
       consultarApi();
     };
     obtenerInforme();
   }, []);
 
-  return(
-    <InformeDiario informe={informe}></InformeDiario>
-  )
+  return <InformeDiario grupos={grupos}></InformeDiario>;
 }
 
 export default App;
--- src/components/InformeDiario.js
+++ src/components/InformeDiario.js
@@ -2,56 +2,197 @@ import React, { Fragment } from "react";
 import { jsPDF } from "jspdf";
 import "jspdf-autotable";
 
-const InformeDiario = ({ informe }) => {
-  function headRows() {
-    return [
-      {
-        nro_afiliado: "AF",
-        apellido: "Apellido",
-        nombre: "Nombre",
-        horarioDesde: "Hora entrada",
-        firmaEntrada: "Firma",
-        horarioHasta: "Hora salida",
-        firmaSalida: "Firma",
-      },
-    ];
-  }
-  const getRowsInforme = () => {
-    let body = [];
-    informe.forEach((i) => {
-      body.push({
-        nro_afiliado: i.nro_afiliado,
-        nombre: i.nombre,
-        apellido: i.apellido,
-        horarioDesde: i.horario_laboral_desde,
-        firmaEntrada: "",
-        horarioHasta: i.horario_laboral_hasta,
-        firmaSalida: "",
-      });
+const InformeDiario = ({ grupos }) => {
+  //Funcion para obtener el grupo
+  function getGrupo(codigoGrupo) {
+    let grupoInforme = {};
+    grupos.map((grupo) => {
+      if (grupo.codigo === codigoGrupo) {
+        grupoInforme = grupo;
+      }
     });
+    return grupoInforme;
+  }
+
+  //Funcion para definir las filas
+  function headRows(tipoTemplate) {
+    if (tipoTemplate === "TEMPLATE_FISCALIZACION") {
+      return [
+        {
+          nro_afiliado: "AF",
+          nombre: "Nombre",
+          apellido: "Apellido",
+          horario: "      ",
+          horarioDesde: "Hora",
+          firmaEntrada: "Firma",
+          horarioHasta: "Hora",
+          firmaSalida: "Firma",
+        },
+      ];
+    }
+    if (tipoTemplate === "TEMPLATE_CONTRATOS_TEMPORARIO") {
+      return [
+        {
+          nro_afiliado: "AF",
+          nombre: "Nombre",
+          apellido: "Apellido",
+          horario: "      ",
+          horarioDesde: "Hora",
+          firmaEntrada: "Firma",
+          horarioHasta: "Hora",
+          firmaSalida: "Firma",
+        },
+      ];
+    }
+  }
+
+  // FUNCION PARA QUE ME DEVUELVA LOS DATOS DEL TEMPLATE
+  // const getDatosTemplate_Fiscalizacion = (tipo, grupo) =>{
+  //   let body = [];
+  //   console.log(grupo)
+  //   if(tipo==="GRUPO"){
+  //     grupo.empleados.map((g) => {
+  //       body.push({
+  //         nro_afiliado: g.nro_afiliado,
+  //         nombre: g.nombre,
+  //         apellido: g.apellido,
+  //         horario: g.horario_laboral,
+  //         horarioDesde: "",
+  //         firmaEntrada: "",
+  //         horarioHasta: "",
+  //         firmaSalida: "",
+  //       });
+  //     });
+  //   }
+
+  //   if(tipo="SUBGRUPO"){
+  //     grupo.subgrupo[0].empleados.map((g) => {
+  //       body.push({
+  //         nro_afiliado: g.nro_afiliado,
+  //         nombre: g.nombre,
+  //         apellido: g.apellido,
+  //         horario: g.horario_laboral,
+  //         horarioDesde: "",
+  //         firmaEntrada: "",
+  //         horarioHasta: "",
+  //         firmaSalida: "",
+  //       });
+  //     });
+  //   }
+  //   return body;
+
+  // }
+
+  //Funcion para obtener los elementos de la tabla
+  const getBodyRowsInforme = (grupo) => {
+    let body = [];
+
+    if (grupo.subgrupo === undefined) {
+      // GRUPO
+      if (grupo.tipo_template === "TEMPLATE_FISCALIZACION") {
+        // getDatosTemplate_Fiscalizacion("GRUPO", grupo)
+        grupo.empleados.map((g) => {
+          body.push({
+            nro_afiliado: g.nro_afiliado,
+            nombre: g.nombre,
+            apellido: g.apellido,
+            horario: g.horario_laboral,
+            horarioDesde: "",
+            firmaEntrada: "",
+            horarioHasta: "",
+            firmaSalida: "",
+          });
+        });
+      }
+      if (grupo.tipo_template === "TEMPLATE_CONTRATOS_TEMPORARIOS") {
+        grupo.empleados.map((g) => {
+          body.push({
+            nro_afiliado: g.nro_afiliado,
+            nombre: g.nombre,
+            apellido: g.apellido,
+            horario: g.horario_laboral,
+            horarioDesde: "",
+            firmaEntrada: "",
+            horarioHasta: "",
+            firmaSalida: "",
+          });
+        });
+      }
+    } else {
+      //SUBGRUPO
+      if (grupo.subgrupo.empleados === undefined) {
+        if (grupo.tipo_template === "TEMPLATE_FISCALIZACION") {
+          grupo.subgrupo.empleados.map((g) => {
+            body.push({
+              nro_afiliado: g.nro_afiliado,
+              nombre: g.nombre,
+              apellido: g.apellido,
+              horario: g.horario_laboral,
+              horarioDesde: "",
+              firmaEntrada: "",
+              horarioHasta: "",
+              firmaSalida: "",
+            });
+          });
+        }
+
+        if (grupo.tipo_template === "TEMPLATE_CONTRATOS_TEMPORARIOS") {
+          console.log(grupo.subgrupo[0].empleados);
+          grupo.subgrupo[0].empleados.map((g) => {
+            body.push({
+              nro_afiliado: g.nro_afiliado,
+              nombre: g.nombre,
+              apellido: g.apellido,
+              horario: g.horario_laboral,
+              horarioDesde: "",
+              firmaEntrada: "",
+              horarioHasta: "",
+              firmaSalida: "",
+            });
+          });
+        }
+      } else {
+        // VALORES `POR DEFECTO`
+      }
+    }
     return body;
   };
 
+  //Funcion para crear el informe
   const crearInformeDiario = () => {
-    console.log(informe);
     const doc = new jsPDF();
-    doc.text("INFORME DE EMPLEADOS", 40,30);
-
-    doc.autoTable({ html: "#my-table" });
+    doc.text("FISCALIZACION", 15, 15);
 
     doc.autoTable({
-      head: headRows(),
-      body: getRowsInforme(),
+      startY: 20,
+      head: headRows("TEMPLATE_FISCALIZACION"),
+      body: getBodyRowsInforme(getGrupo(3)),
       theme: "grid",
     });
+
+    let pageNumber = doc.internal.getNumberOfPages();
+
+    doc.text("CONTRATOS TEMPORARIOS", 15, 85);
+    doc.autoTable({
+      startY: 90,
+      showHead: "firstPage",
+      head: headRows("TEMPLATE_CONTRATOS_TEMPORARIO"),
+      body: getBodyRowsInforme(getGrupo(2)),
+      theme: "grid",
+    });
+
+    doc.setPage(pageNumber);
     doc.save("InformeDiario.pdf");
   };
 
   return (
     <Fragment>
       <div className="container text-center">
-        <h1 className="mt-5 text-center">Informe diario</h1>
-        <button className="btn btn-primary mt-2" onClick={crearInformeDiario}>
+        <h1 className="mt-5 text-center display-2">Informe diario</h1>
+        <button
+          className="btn btn-primary mt-2 w-100"
+          onClick={crearInformeDiario}
+        >
           Obtener Informe
         </button>
       </div>
